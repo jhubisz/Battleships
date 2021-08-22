c0d4aacd1d55b4b5d10516595f594971999d77bc
Jakub Hubisz <jakub.hubisz@gmail.com>
2021-08-22 14:05:04 +0200

    Initial commit

---
+++ BattleshipTests/BoardTest.cs
@@ -0,0 +1,14 @@
+using Battleship;
+using Xunit;
+
+namespace BattleshipTests
+{
+    public class BoardTest
+    {
+        [Fact]
+        public void BoardExists()
+        {
+            var board = new Board();
+        }
+    }
+}
