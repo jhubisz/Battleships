0b1e75b13ba48b05eb635dce4a4734b5839d3319
Jakub Hubisz <jakub.hubisz@gmail.com>
2021-08-28 11:26:15 +0200

    Adding boards fields

--- Battleship/Board.cs
+++ Battleship/Board.cs
@@ -2,5 +2,14 @@
 {
     public class Board
     {
+        const int DEFAULT_SIZE = 10;
+
+        public int[,] Fields { get; set; }
+
+        public Board() : this(DEFAULT_SIZE) { }
+        public Board(int size)
+        {
+            Fields = new int[size, size];
+        }
     }
 }
--- BattleshipTests/BoardTest.cs
+++ BattleshipTests/BoardTest.cs
@@ -1,14 +1,23 @@
 using Battleship;
+using System;
 using Xunit;
 
 namespace BattleshipTests
 {
     public class BoardTest
     {
-        [Fact]
-        public void BoardExists()
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        public void BoardFieldsPropHasCorrectSize(int size)
         {
-            var board = new Board();
+            var board = new Board(size);
+            var fields = board.Fields;
+            const int NO_OF_BOARD_DIMENSIONS = 2;
+            Assert.Equal(NO_OF_BOARD_DIMENSIONS, fields.Rank);
+            Assert.Equal(size, fields.GetUpperBound(0) + 1);
+            Assert.Equal(size, fields.GetUpperBound(1) + 1);
         }
     }
 }
