8770e3a0249fd905323106ceebf44bce2786bfac
Jakub Hubisz <jakub.hubisz@gmail.com>
2021-08-28 15:14:33 +0200

    Adding ships to board

--- Battleship/Board.cs
+++ Battleship/Board.cs
@@ -1,15 +1,40 @@
-﻿namespace Battleship
+﻿using Battleship.Enums;
+using Battleship.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Battleship
 {
     public class Board
     {
         const int DEFAULT_SIZE = 10;
 
-        public int[,] Fields { get; set; }
+        public IPlacable[,] Fields { get; set; }
+
+        public List<Ship> Ships { get; set; }
 
         public Board() : this(DEFAULT_SIZE) { }
         public Board(int size)
         {
-            Fields = new int[size, size];
+            Fields = new IPlacable[size, size];
+            Ships = new List<Ship>();
+        }
+
+        public Ship PlaceShip(int shipLength, (int x, int y) initialPosition, ShipDirection direction)
+        {
+            var ship = new Ship(shipLength);
+            ship.Place(initialPosition, direction);
+
+            Ships.Add(ship);
+            PopulateShipFields(ship);
+            
+            return ship;
+        }
+
+        private void PopulateShipFields(Ship ship)
+        {
+            foreach (var field in ship.Fields)
+                Fields[field.x - 1, field.y - 1] = ship;
         }
     }
 }
---
+++ BattleshipTests/ShipOnBoardPlacementTests.cs
@@ -0,0 +1,64 @@
+﻿using Battleship;
+using Xunit;
+using Battleship.Enums;
+
+namespace BattleshipTests
+{
+    public class ShipOnBoardPlacementTests
+    {
+        private const int SHIP_LENGTH = 3;
+        public Board Board { get; set; }
+
+        public ShipOnBoardPlacementTests()
+        {
+            Board = new Board();
+        }
+
+        [Fact]
+        public void BoardAllowsPlacingOfAShip()
+        {
+            var shipLength = 1;
+            var initialPosition = (x: 1, y: 1);
+            var ship = Board.PlaceShip(shipLength, initialPosition, ShipDirection.Horizontal);
+
+            Assert.NotNull(ship); 
+        }
+
+        [Fact]
+        public void BoardAllowsPlacingOfA3LengthHorizontalShip()
+        {
+            var shipLength = 3;
+            var initialPosition = (x: 1, y: 1);
+            var ship = Board.PlaceShip(shipLength, initialPosition, ShipDirection.Horizontal);
+
+            Assert.Equal(ship, Board.Fields[0, 0]);
+            Assert.Equal(ship, Board.Fields[1, 0]);
+            Assert.Equal(ship, Board.Fields[2, 0]);
+        }
+
+        [Fact]
+        public void BoardAllowsPlacingOfA3LengthVerticalShip()
+        {
+            var shipLength = 3;
+            var initialPosition = (x: 1, y: 1);
+            var ship = Board.PlaceShip(shipLength, initialPosition, ShipDirection.Vertical);
+
+            Assert.Equal(ship, Board.Fields[0, 0]);
+            Assert.Equal(ship, Board.Fields[0, 1]);
+            Assert.Equal(ship, Board.Fields[0, 2]);
+        }
+
+        [Fact]
+        public void BoardShipsCollectionContainsShipsAfterAddingToBoard()
+        {
+            var shipLength = 3;
+            var initialPosition = (x: 1, y: 3);
+            var ship = Board.PlaceShip(shipLength, initialPosition, ShipDirection.Horizontal);
+            var ship2 = Board.PlaceShip(shipLength, initialPosition, ShipDirection.Horizontal);
+
+            Assert.Collection(Board.Ships,
+                item => Assert.Equal(ship, item),
+                item => Assert.Equal(ship2, item));
+        }
+    }
+}
